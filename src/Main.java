37aa93e867dd1d45967d1df57c1038ea3dab7c7f
Emerson Arévalo <AO22002@ues.edu.sv>
2024-09-28 10:16:04 -0600

    Trabajando en develop

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         String miTexto = "mundo";
         System.out.println("Hola "+ miTexto);
+        System.out.println("Git y Github pratice");
     }
 }
